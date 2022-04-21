efc28b25bdd8c1b38ddff51764189b10d28e0784
MasterKtos <236669@edu.p.lodz.pl>
2022-04-21 15:15:41 +0200

    Make balls move

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -30,7 +30,9 @@ namespace TPW.Presentation.ViewModel
             }
         }
 
-        private string debug
+        // DEBUG
+        public ObservableCollection<DebugString> DebugTxt { get; set; }
+
         public ICommand IncreaseButton { get; }
         public ICommand DecreaseButton { get; }
         public ICommand StartSimulationButton { get; }
@@ -56,6 +58,13 @@ namespace TPW.Presentation.ViewModel
             {
                 model.SetBallNumber(BallsCount);
 
+                //DEBUG
+                DebugTxt = new ObservableCollection<DebugString>();
+                for (int i = 0; i < BallsCount; i++)
+                {
+                    DebugTxt.Add(new DebugString());
+                }
+
                 for (int i = 0; i < BallsCount; i++)
                 {
                     Circles.Add(new BallPosition());
@@ -63,16 +72,18 @@ namespace TPW.Presentation.ViewModel
 
                 model.BallPositionChange += (sender, argv) =>
                 {
-                    Circles[argv.ball.id].ChangePosition(argv.ball.Position);
+                    Circles[argv.ball.id]?.ChangePosition(argv.ball.Position);
+                    // DEBUG
+                    DebugTxt[argv.ball.id].Value = argv.ball.id + ": " + Circles[argv.ball.id].ToString();
                 };
                 model.StartSimulation();
             });
             StopSimulationButton = new RelayCommand(() =>
             {
                 model.StopSimulation();
+                Circles.Clear();
+                model.SetBallNumber(BallsCount);
             });
-
-
         }
 
         // Event for View update
@@ -81,6 +92,23 @@ namespace TPW.Presentation.ViewModel
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(caller));
         }
+    }
+}
+
+//DEBUG
+public class DebugString : INotifyPropertyChanged
+{
+    private string _Value;
+    public string Value
+    {
+        get { return _Value; }
+        set { _Value = value; OnPropertyChanged("Value"); }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected virtual void OnPropertyChanged([CallerMemberName] string caller = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(caller));
     }
 }
 
@@ -179,18 +207,18 @@ public class AsyncObservableCollection<T> : ObservableCollection<T>
     }
 }
 
-public class BallPosition
+public class BallPosition : INotifyPropertyChanged
 {
     private Vector2 pos;
     public float X
     {
         get { return pos.X; }
-        set { pos.X = value; }
+        set { pos.X = value; OnPropertyChanged(); }
     }
     public float Y
     {
         get { return pos.Y; }
-        set { pos.Y = value; }
+        set { pos.Y = value; OnPropertyChanged(); }
     }
 
     public BallPosition(float x, float y)
@@ -215,4 +243,15 @@ public class BallPosition
         this.X = position.X;
         this.Y = position.Y;
 	}
+
+	public override string ToString()
+	{
+        return $"({X}, {Y})";
+	}
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected virtual void OnPropertyChanged([CallerMemberName] string caller = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(caller));
+    }
 }
